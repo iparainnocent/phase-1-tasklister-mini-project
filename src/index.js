056a962102f4dfe316fe32eb5aea9032cc983f0f
iparainnocent <innocent.ipara@student.moringaschool.com>
2024-07-12 09:10:43 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const form = document.getElementById('create-task-form');
+
+  
+  form.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+  
+    
+    const taskDescription = document.getElementById('new-task-description').value;
+  
+    
+    const newTaskItem = document.createElement('li');
+  
+    
+    newTaskItem.textContent = taskDescription;
+  
+  
+    const tasksList = document.getElementById('tasks');
+  
+    
+    tasksList.appendChild(newTaskItem);
+  
+    
+    document.getElementById('new-task-description').value = '';
+  });
+
+ 
 });
